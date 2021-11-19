4c571582fb30d37ec1f83da8dd99960defe4d448
nevriye <nevriyetasim@hotmail.com>
2021-11-20 01:23:39 +0300

    added dropdown comp

--- src/moduls/movies/components/DropDown.vue
+++ src/moduls/movies/components/DropDown.vue
@@ -1,19 +1,32 @@
 <template>
-   <div class="col-6 col-md-4">
-    <select class="form-select" aria-label="Default select example">
-  <option selected>Sırala</option>
-  <option value="1">Yeniye Göre Sırala</option>
-  <option value="2">Eskiye Göre Sırala</option>
-  <option value="3">Puana Göre Sırala</option>
-  <option value="3">Rastgele Sırala</option>
-</select>
-    </div>
+  <div class="col-6 col-md-4">
+    <select  v-on:change="change($event)">
+    <option>Sırala</option>
+    <option value="1">Yeniye Göre Sırala</option>
+    <option value="2">Eskiye Göre Sırala</option>
+     <option value="3">Puana Göre Sırala</option>
+    <option value="4">Rastgele Sırala</option>
+  </select>
+  <p>{{selected}}</p>
+</div>
 
-
-            
 </template>
 <script>
 export default {
   name: "DropDown",
+  data(){
+  return{ 
+     selected: null
+  }
+  },
+   methods: {
+     change(event)
+     { this.selected = event.target.value 
+      // this.$emit("seriess", this.selected);
+    
+       // const sortedActivities = activities.slice().sort((a, b) => b.date - a.date)
+       }
+  
+  },
 };
 </script>
